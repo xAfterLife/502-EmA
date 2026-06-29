7c57bae469eb67436edc76adc6e1ed622f96ee1e
jonshor <jonas.horwedel@gmail.com>
2026-06-29 17:37:07 +0200

    encryption initial commit

---
+++ min_vault/lib/core/crypto/encryption_service.dart
@@ -0,0 +1,12 @@
+
+class EncryptionService {
+  EncryptionService._();
+
+  static Future<void> encryptFile(String filePath) async {
+
+  }
+
+  static Future<void> decryptFile(String filePath) async {
+    
+  }
+}
